0d112c02454a9e9238fe002d26bcbbb7f31cac1c
Michael Black <mwboct16@gmail.com>
2019-06-05 18:54:59 -0400

    Initial Push

---
+++ clicky-game/src/images.js
@@ -0,0 +1,29 @@
+import gon from './images/gon.jpg';
+import itachi from './images/itachi.png';
+import jerry from './images/jerry.jpg';
+import mickey from './images/mickey.jpg';
+import Naruto from './images/Naruto.png';
+import pikachu from './images/pikachu.jpg';
+import Samurai_Jack from './images/Samurai_Jack.png';
+import Sasuke from './images/Sasuke.png';
+import spongebob from './images/spongebob.jpg';
+import supermario from './images/supermario.jpg';
+import tina from './images/tina.jpg';
+import Zabuza from './images/Zabuza.png';
+
+const images = [
+    gon,
+    itachi,
+    jerry,
+    mickey,
+    Naruto,
+    pikachu,
+    Samurai_Jack,
+    Sasuke,
+    spongebob,
+    supermario,
+    tina,
+    Zabuza
+];
+
+export default images;
